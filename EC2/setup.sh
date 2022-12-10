e0a3b547028744a13ef34d13bb5133b9faf71461
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-12-10 11:46:10 -0500

    dotnet install

--- EC2/setup.sh
+++ EC2/setup.sh
@@ -4,4 +4,4 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get update -y
-sudp apt install nginx nodejs npm -y
+sudp apt install nginx nodejs npm certbot python3-certbot-nginx aspnetcore-runtime-6.0 dotnet-sdk-6.0 -y
