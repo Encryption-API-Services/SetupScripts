8788235a3e05d2d0c697cf6f9b8883a33636d1b7
Mike Mulchrone <mtmulch0191@outlook.com>
2022-12-12 20:46:22 -0500

    adding sudo

--- Deploy/deploy-api.sh
+++ Deploy/deploy-api.sh
@@ -5,6 +5,6 @@ sudo git pull
 cd API
 sudo systemctl stop API
 sudo rm -rf /srv/api/*
-dotnet publish --configuration Release -o /srv/api
+sudo dotnet publish --configuration Release -o /srv/api
 sudo systemctl daemon-reload
 sudo systemctl start API
